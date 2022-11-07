ae6fb6d2ac3eb6631f788e8709af751e337b73f1
Khushi Gupta <84071330+khushigupta02@users.noreply.github.com>
2022-11-07 12:23:09 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,12 @@
-hi i am khushi gupta and performing codepipeline experiment.
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Khushi Gupta</title>
+</head>
+<body>
+        <h1><?php echo 'hi i am khushi gupta and performing codepipeline experiment.'; ?></h1>
+</body>
+</html>
